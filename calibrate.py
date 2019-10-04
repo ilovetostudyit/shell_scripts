9663fd90f4e0a87a44e58faf19dff7636c913e53
Sonia Lukyanova <ilovetostudyit@gmail.com>
2019-10-04 23:36:22 +0300

    Update calibrate.py

--- calibrate.py
+++ calibrate.py
@@ -100,7 +100,7 @@ text = "ROI is not setted"
 while(True):
     # Capture frame-by-frame
 #    frame = cap.read()
-    frame = cv2.imread("images\ROI_6.jpg")
+    frame = cv2.imread("shell_scripts/images/ROI_6.jpg")
     clone = frame.copy()
     printtext(frame, text)
     cv2.namedWindow("frame")
@@ -125,7 +125,7 @@ while(True):
         break
     elif key == ord("s") and calibrate == False:
         try:
-            cv2.imwrite( "images/ROI_" + str(a) +".jpg", roi)
+            cv2.imwrite( "shell_scripts/images/ROI_" + str(a) +".jpg", roi)
             a = a + 1
             text = "ROI SAVED"
             saved = True
